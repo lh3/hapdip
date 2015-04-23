ad92636ecaee933a23a3e2a9ab300602fe013dae
Heng Li <lh3@me.com>
2015-04-23 16:57:23 -0400

    added vcf2bed

--- hapdip.js
+++ hapdip.js
@@ -904,6 +904,92 @@ function b8_filter(args)
 	buf.destroy();
 }
 
+/********************************
+ *** Convert VCF to unary BED ***
+ ********************************/
+
+// the following is similar to b8_parse_vcf1
+function b8_parse_vcf_multi(t) // t = vcf_line.split("\t")
+{
+	if (t.length < 6) return null;
+	var a = [];
+	t[1] = parseInt(t[1]) - 1; t[3] = t[3].toUpperCase(); t[4] = t[4].toUpperCase(); t[5] = parseFloat(t[5]);
+	var s = t[4].split(","); // list of ALT alleles
+	// get allele counts
+	var n_alleles = t[4].split(",").length + 1, n_gt = Math.floor(n_alleles * (n_alleles + 1) / 2 + .499);
+	var ACA = [], tot = 0;
+	for (var i = 0; i < n_alleles; ++i) ACA[i] = 0;
+	if (t.length >= 10 && t[8].substr(0, 2) == 'GT') {
+		for (var i = 9; i < t.length; ++i) {
+			var m;
+			if ((m = /(\.|\d+)[\/\|](\.|\d+)/.exec(t[i])) != null) {
+				if (m[1] != '.') ++ACA[parseInt(m[1])], ++tot;
+				if (m[2] != '.') ++ACA[parseInt(m[2])], ++tot;
+			}
+		}
+	}
+	// get CIGAR for freebayes
+	var m3 = /CIGAR=([^;\t]+)/.exec(t[7]);
+	var cigar = m3 != null? m3[1].split(",") : [];
+	if (cigar.length && cigar.length != s.length) throw Error("Inconsistent ALT and CIGAR");
+	// loop through each ALT allele
+	for (var i = 0; i < s.length; ++i) {
+		if (t[3].length == 1 && s[i].length == 1) { // SNP
+			if (t[3] != s[i]) a.push([t[1], t[1]+1, 0, t[3], s[i], ACA[i+1], tot]);
+		} else if (cigar.length) { // MNP or INDEL from freebayes
+			var x = 0, y = 0;
+			var m4, re = /(\d+)([MXID])/g;
+			while ((m4 = re.exec(cigar[i])) != null) {
+				var l = parseInt(m4[1]);
+				if (m4[2] == 'X') {
+					for (var j = 0; j < l; ++j) {
+						var u = t[3].substr(x+j, 1), v = s[i].substr(y+j, 1);
+						a.push([t[1] + x, t[1]+x+1, 0, u, v, ACA[i+1], tot]);
+					}
+					x += l, y += l;
+				} else if (m4[2] == 'I') {
+					if (x == 0 || y == 0) throw Error("Leading I/D");
+					var u = t[3].substr(x-1, 1), v = s[i].substr(y-1, l+1);
+					a.push([t[1] + x - 1, t[1]+x, l, u, v, ACA[i+1], tot]);
+					y += l;
+				} else if (m4[2] == 'D') {
+					if (x == 0 || y == 0) throw Error("Leading I/D");
+					var u = t[3].substr(x-1, l+1), v = s[i].substr(y-1, 1);
+					a.push([t[1] + x - 1, t[1]+x+l, -l, u, v, ACA[i+1], tot]);
+					x += l;
+				} else if (m4[2] == 'M') x += l, y += l;
+			}
+		} else { // MNP or INDEL from Platypus and others
+			var l = t[3].length < s[i].length? t[3].length : s[i].length;
+			for (var j = 0; j < l; ++j) { // decompose long variants
+				var u = t[3].substr(j, 1), v = s[i].substr(j, 1);
+				if (u != v) a.push([t[1] + j, t[1]+j+1, 0, u, v, ACA[i+1], tot]);
+			}
+			var d = s[i].length - t[3].length;
+			if (d != 0) a.push([t[1] + l - 1, t[1] + t[3].length, d, t[3].substr(l-1), s[i].substr(l-1), ACA[i+1], tot]);
+		}
+	}
+	return a; // [start, end, indelLen, ref, alt, AC, tot]
+}
+
+function b8_vcf2bed(args)
+{
+	var file = args.length == 0? new File() : new File(args[0]);
+	var buf = new Bytes();
+
+	while (file.readline(buf) >= 0) {
+		var line = buf.toString();
+		if (line.charAt(0) == '#') continue;
+		var t = line.split("\t");
+		var a = b8_parse_vcf_multi(t);
+		for (var i = 0; i < a.length; ++i)
+			print(t[0], a[i].join("\t"), t[6]);
+	}
+
+	buf.destroy();
+	file.close();
+}
+
 /**********************************
  *** Evaluate CHM1-NA12878 VCFs ***
  **********************************/
@@ -1131,6 +1217,7 @@ function main(args)
 		print("          filter   filter anno'd VCF");
 		print("");
 		print("          qst1     vcf stats stratified by QUAL, one sample only");
+		print("          vcf2bed  convert VCF to unary BED");
 		print("          cg2vcf   convert CG's masterVarBeta to VCF");
 		print("          bedovlp  count lines overlapping in a second bed");
 		print("          bedcmpm  compare multiple sorted BED files");
@@ -1146,6 +1233,7 @@ function main(args)
 	else if (cmd == 'anno') b8_anno(args);
 	else if (cmd == 'filter') b8_filter(args);
 	else if (cmd == 'qst1') b8_qst1(args);
+	else if (cmd == 'vcf2bed') b8_vcf2bed(args);
 	else if (cmd == 'cg2vcf') b8_cg2vcf(args);
 	else if (cmd == 'bedovlp') b8_bedovlp(args);
 	else if (cmd == 'bedcmpm') b8_bedcmpm(args);
